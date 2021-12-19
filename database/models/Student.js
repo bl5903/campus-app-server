2a60b6fa3272835fe5b57fb5be283e8989aafa1d
bl5903 <bql1388@gmail.com>
2021-12-18 21:11:08 -0500

    update
    Updated the campus, student, and seed.js files

--- database/models/Student.js
+++ database/models/Student.js
@@ -11,8 +11,28 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  email:{
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate:{isEmail: true}
+  },
+
+  imageUrl:{
+    type: Sequelize.STRING,
+    defaultvalue:"https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg",
+    validate:{isURL: true}
+  },
+
+  gpa:{
+    type:Sequelize.DECIMAL,
+    validate:{min: 0.0, max: 4.0}
   }
 
 });
 
 module.exports = Student;
+
+
+//https://sequelize.org/master/manual/validations-and-constraints.html
