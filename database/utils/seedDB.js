6de8b6436be8aa36a1891e05a6470babbe0ae3e7
bl5903 <bql1388@gmail.com>
2021-12-21 11:36:57 -0500

    Update Seeddb.js
    Put new changes and hunter img

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,20 +3,48 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
-		description: "This is a school in NYC."
+		description: "This is a school in NYC.",
+		imageUrl:"https://s29068.pcdn.co/wp-content/uploads/hunter-campus.jpg",
+		address:"695 Park Ave, New York, NY 10065"
 	});
+
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
-		description: "This is a school in MA."
+		description: "This is a school in MA.",
+		imageUrl:"https://college.harvard.edu/sites/default/files/styles/billboard_image/public/2019-05/fpo-linderpix-harvard-58118.jpg?itok=x-OnBrai",
+		address:"University Hall Cambridge, MA 02138"
+	});
+
+	const dummy_campus3 = await Campus.create({
+		name: "University of Alabama",
+		description: "This is a school in AL.",
+		imageUrl:"https://newcollege.ua.edu/wp-content/uploads/2020/04/LloydHall2-1.jpg",
+		address:"Tuscaloosa, AL 35487"
+	});
+
+	const dummy_campus4 = await Campus.create({
+		name: "University of Oklahoma",
+		description: "This is a school in OK.",
+		imageUrl:"https://www.visitnorman.com/uploads/The-University-of-Oklahoma/Campus/ou_lib.jpg",
+		address:"660 Parrington Oval, Norman, OK 73019"
+	});
+
+	const dummy_campus5 = await Campus.create({
+		name: "Coastal Carolina University",
+		description: "This is a school in SC.",
+		imageUrl:"https://www.cappex.com/sites/default/files/styles/college_hero_desktop/public/images/hero/college/218724_hero.jpg?itok=Xn0Kof4P",
+		address:"100 Chanticleer Dr E, Conway, SC 29528"
 	});
 
 	const dummy_student = await Student.create({
-			firstname: "Joe",
-      lastname: "Shmo",
-		});
+			firstname: "Jaelen",
+			lastname: "Jones",
+			email: "triplejragereader@aol.com",
+			gpa: 2.47
+	});
 
-	await dummy_student.setCampus(dummy_campus);
-	
+		await dummy_student.setCampus(dummy_campus);
+
 }
 
 module.exports = seedDB;
